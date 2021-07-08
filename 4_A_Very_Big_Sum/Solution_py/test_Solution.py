5b78df168f6521e870c6d9c3bc0c0723af91648c
Napatchol Thaipanich <patorseing@gmail.com>
2021-07-08 08:44:05 +0700

    4: python

---
+++ 4_A_Very_Big_Sum/Solution_py/test_Solution.py
@@ -0,0 +1,9 @@
+import unittest
+import Solution
+
+class SolveTestCase(unittest.TestCase):
+  def testCaseI(self):
+    ar = list(map(int, "1000000001 1000000002 1000000003 1000000004 1000000005".split()))
+    actual = Solution.aVeryBigSum(ar)
+    excepted = 5000000015
+    self.assertEqual(actual, excepted)
